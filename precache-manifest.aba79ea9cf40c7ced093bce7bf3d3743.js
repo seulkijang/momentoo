5ecd9d74afdfce69b43a52b80cabddee1f86ce44
seulki <seulki.dev@gmail.com>
2020-01-16 14:08:46 -0500

    Updates

---
+++ precache-manifest.aba79ea9cf40c7ced093bce7bf3d3743.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dc2f9fade42158ba63e",
+    "url": "/momentoo/static/js/main.3dc2f9fa.chunk.js"
+  },
+  {
+    "revision": "f2c1e6c665e9e5b57659",
+    "url": "/momentoo/static/js/1.f2c1e6c6.chunk.js"
+  },
+  {
+    "revision": "2353b53c80def836777d",
+    "url": "/momentoo/static/js/runtime~main.2353b53c.js"
+  },
+  {
+    "revision": "df120058b4fc2294dcfce0ebb17edf33",
+    "url": "/momentoo/index.html"
+  }
+];
